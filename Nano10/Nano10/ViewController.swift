1dd429429e0fc26fe682ea5a46f4c7aeb1656270
Lucas Alexandre Amorim Leoncio <77064352+Lucas-AlexandreL@users.noreply.github.com>
2022-07-18 14:59:35 -0300

    Initial commit

---
+++ Nano10/Nano10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano10
+//
+//  Created by Lucas Alexandre Amorim Leoncio on 18/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
